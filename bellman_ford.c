14882c14f4a08a64d4b57187f8b8ffd391abe00f
vishnu <vishnu44d@gmail.com>
2019-04-07 23:44:24 +0530

    bellman ford started

---
+++ bellman_ford.c
@@ -0,0 +1,68 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+#define MAX 20
+#define INFINITY 999999
+#define True 1
+#define False 0
+
+// DEINATION of GRAPH (Adj. Matrix) :: BEGIN --
+void create_graph(int (*graph)[MAX], int n)
+{
+    int i, j;
+    for(i=0; i<n; i++)
+    {
+        for(j=0; j<n; j++)
+            graph[i][j] = INFINITY;
+    }
+    printf("GRAPH created with number of Vertex: %d\n", n);
+}
+
+void add_edge(int (*graph)[MAX], int u, int v, int w)
+{
+    graph[u][v] = w;
+    printf("Edge Added %d ~ %d with weight = %d\n", u, v, w);
+}
+
+void del_edge(int (*graph)[MAX], int u, int v)
+{
+    graph[u][v] = INFINITY;
+    printf("Edge Deleted %d ~ %d \n", u, v);
+}
+
+void get_adj(int (*graph)[MAX], int n, int u, int *adj, int *size)
+{
+    //size is the number of vertex adjecent to `u`
+    int i, j=0;
+    for(i=0;i<n;i++)
+    {
+        if(graph[u][i] != INFINITY)
+        {
+            adj[j] = i;
+            j++;
+        }
+            
+    }
+    *size = j;
+        
+}
+
+void print_graph(int (*graph)[MAX], int n)
+{
+    int i, j;
+    for(i=0; i<n; i++)
+    {
+        for(j=0; j<n; j++)
+        {
+            if(graph[i][j] == INFINITY)
+                printf("INF\t");
+            else
+                printf("%d\t",graph[i][j]);
+        }
+            
+        printf("\n");
+    }
+}
+
+// DEINATION of GRAPH (Adj. Matrix) :: END --
+
